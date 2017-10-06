f6bb0567141830848a9d9a69b984b3bd7274e235
Callum Hibbert <callum.hibbert@gmail.com>
2017-10-06 12:44:02 +0100

    Scripts for working "onmytwenty-web-ui" deployment with Route53 and S3 hosting.

--- onmytwenty-web-ui-Stack-Create.cmd
+++ onmytwenty-web-ui-Stack-Create.cmd
@@ -1,2 +1,2 @@
 call onmytwenty-web-ui-Stack-Upload.cmd
-call aws cloudformation create-stack --stack-name onmytwenty-web-ui --disable-rollback --template-url https://s3-eu-west-1.amazonaws.com/onmytwenty-cloudformation/onmytwenty-web-ui.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=EnvironmentName,ParameterValue=Test
+call aws cloudformation create-stack --stack-name onmytwenty-web-ui --disable-rollback --template-url https://s3-eu-west-1.amazonaws.com/onmytwenty-cloudformation/onmytwenty-web-ui.yml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=EnvironmentName,ParameterValue=Test ParameterKey=RootDomainName,ParameterValue=onmytwenty.com
